82b8a74c11d4bc8f5681fc9743742a3aae3f2079
Surya-Mukherjee <suryamukherjee67@gmail.com>
2025-06-28 17:25:01 +0530

    added milliseconds

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ function updateClock(){
     hours=hours.toString().padStart(2,0)
     const minutes=now.getMinutes().toString().padStart(2,0);
     const seconds=now.getSeconds().toString().padStart(2,0);
-    const timeString=`${hours}:${minutes}:${seconds} ${meridian}`;
+    const milliSeconds=now.getMilliseconds().toString().padStart(2,0);
+    const timeString=`${hours}:${minutes}:${seconds}:${milliSeconds} ${meridian}`;
     document.getElementById("clock").textContent=timeString;
 }
 updateClock();
